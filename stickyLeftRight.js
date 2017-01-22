ff3f00b19b555a5a0f62a7d33727d6d842d1e27e
kabarnusa.com <kabarnusacom@gmail.com>
2017-01-22 10:20:14 +0700

    Add files via upload

---
+++ stickyLeftRight.js
@@ -0,0 +1,57 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(function(){ // document ready
+   if (!!$('#newspro').length) { // make sure "#sticky" element exists
+      var el = $('#newspro');
+      var stickyTop = $('#newspro').offset().top; // returns number
+      var footerTop = $('#stopfixed').offset().top; // returns number
+      var stickyHeight = $('#newspro').height();
+      var limit = footerTop - stickyHeight - 50;
+      $(window).scroll(function(){ // scroll event
+          var windowTop = $(window).scrollTop(); // returns number
+            
+          if (stickyTop < windowTop){
+             el.css({ position: 'fixed', top: 0 });
+          }
+          else {
+             el.css('position','static');
+          }
+            
+          if (limit < windowTop) {
+          var diff = limit - windowTop;
+          el.css({top: diff});
+          }     
+        });
+   }
+});
+//]]>
+</script>
+
+<script type='text/javascript'>
+//<![CDATA[
+$(function(){ // document ready
+   if (!!$('#sidebar').length) { // make sure "#sticky" element exists
+      var el = $('#sidebar');
+      var stickyTop = $('#sidebar').offset().top; // returns number
+      var footerTop = $('#stopfixed').offset().top; // returns number
+      var stickyHeight = $('#sidebar').height();
+      var limit = footerTop - stickyHeight - 50;
+      $(window).scroll(function(){ // scroll event
+          var windowTop = $(window).scrollTop(); // returns number
+            
+          if (stickyTop < windowTop){
+             el.css({ position: 'fixed', top: 0 });
+          }
+          else {
+             el.css('position','static');
+          }
+            
+          if (limit < windowTop) {
+          var diff = limit - windowTop;
+          el.css({top: diff});
+          }     
+        });
+   }
+});
+//]]>
+</script>
